ecb49ffbf436548c083b6e7600785f77b1651b06
anniep8911 <pjatopazz@gmail.com>
2022-06-02 20:58:49 +0900

    ver03

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ function main(){
     const slideRight = document.querySelector('.cnt01 .btnR');
     const slideLeft = document.querySelector('.cnt01 .btnL');
     const slideGr = document.querySelector('.cnt01 .artGroup');
+    const cnt3Btn =  document.querySelector('.cnt03 button');
+    const cnt2more = document.querySelector('.cnt02>.btn>button');
 
+    let [crnPg,flag,hg,cnt] = [0,false,0,1];
 
-    let crnPg = 0;
     function pgNation(ind){
         for(let i=0; i< pg.length;i++){
             mnImg[i].classList.remove('on');
@@ -15,53 +17,39 @@ function main(){
         mnImg[ind].className= mnImg[ind].className+' on';
         pg[ind].className ='clicked';
     }
-
-    function slide(dir){
-        if(dir=='left'){
-            slideGr.animate({
-                marginLeft:'-66.66%'
-            },{
-                duration:500,
-                fill:'forwards'
-            });
-
-            setTimeout(function(){
-                slideGr.animate({
-                    marginLeft:'-33.33%'
-                },{
-                    duration:0,
-                    fill:'forwards'
-                });
-                slideGr.append(slideGr.firstElementChild);
-            },500);
-        }else{
-            slideGr.animate({
-                marginLeft:'-0%'
-            },{
-                duration:500,
-                fill:'forwards'
-            });
     
-            setTimeout(function(){
-                slideGr.animate({
-                    marginLeft:'-33.33%'
-                },{
-                    duration:0,
-                    fill:'forwards'
-                });
-                slideGr.prepend(slideGr.lastElementChild);
-            },500);  
-        }
+    function back(dir,gr,bgSet){            
+        setTimeout(function(){
+            gr.animate({
+                marginLeft:bgSet
+            },{
+                duration:0,
+                fill:'forwards'
+            });
+            if(dir == 'left'){
+                gr.append(gr.firstElementChild);
+            }else{
+                gr.prepend(gr.lastElementChild);
+            }
 
+        },500);
+    }
+    
+    function slide(dir,deg,gr,bgSet){
+        gr.animate({
+            marginLeft:deg
+        },{
+            duration:500,
+            fill:'forwards'
+        });
+        back(dir,gr,bgSet);
     }
 
-    function more(){
-        
+    function more(cnt,gr,endInd,btn){
+        if(!flag) hg = gr.clientHeight;
+        if(cnt<=endInd)gr.style.height = `${hg*cnt}px`;
+        else btn.innerText='close';
     }
-
-    slideRight.onclick=()=>{slide('right')};
-    slideLeft.onclick=()=>{slide('left')};
-
 
     const autoRolling = setInterval(function(){
         crnPg++;
@@ -69,17 +57,18 @@ function main(){
         pgNation(crnPg);
     },1500);
 
-
+    console.log(document.querySelector('.cnt02 section'));
+    slideRight.onclick=()=>{slide('right','0%',slideGr,'-33.33%')};
+    slideLeft.onclick=()=>{slide('left','-66.66%',slideGr,'-33.33%')};
+    cnt3Btn.onclick=()=>{slide('left','-50%',document.querySelector('.cnt03 .artGroup'),'-25%')};
+    cnt2more.onclick=(e)=>{cnt++;more(cnt,document.querySelector('.cnt02 section'),2,e.currentTarget); flag=true;};
+    
     pg.forEach((e,ind)=>{
-        e.onclick=(e)=>{
+        e.onclick=()=>{
             pgNation(ind);
             clearInterval(autoRolling);
         }
     });
 
-
-
-
 }
-
 export default main;
