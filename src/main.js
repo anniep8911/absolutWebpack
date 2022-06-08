b34a9a7b17a1c24ccc292eb73abf1240747f7479
anniep8911 <pjatopazz@gmail.com>
2022-06-08 10:20:39 +0900

    mobile

--- src/main.js
+++ src/main.js
@@ -45,7 +45,11 @@ function main(){
     });
 
     if(fnc.wd<=1024){
-        fnc.touch(slideGr,'-66.66%','-33.33%');
+        if(fnc.wd <= 700){
+            fnc.touch(slideGr,'-50%','-25%');
+        }else{
+            fnc.touch(slideGr,'-100%','-50.33%');
+        }
         fnc.touch(document.querySelector('.cnt03 .artGroup'),'-40%','-20%');
         fnc.touch(document.querySelector('.cnt02 .artGroup'),'-100%','-50%');
     }
